a3c7c03f5e7d39145674f28a7620952d2169b36e
sara_rodrigues <sara_rodrigues@SNFNS2EP13VM050.florianopolis.sc.senai.br>
2014-09-16 19:46:57 -0300

    Bandajpa(corrigido)

--- introducaojpa/src/br/senai/sc/ti20131n/pw/introducaojpa/util/JpaUtil.java
+++ introducaojpa/src/br/senai/sc/ti20131n/pw/introducaojpa/util/JpaUtil.java
@@ -1,4 +1,4 @@
-package br.senai.sc.ti20131n.pw.JpaUtil;
+package br.senai.sc.ti20131n.pw.introducaojpa.util;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -7,19 +7,18 @@ import javax.persistence.Persistence;
 public class JpaUtil {
 	private static EntityManagerFactory entityManagerFactory;
 	
-	public static void initFactory(){
-		if(entityManagerFactory != null)return;
-		entityManagerFactory = Persistence
+	public static void initFactory() {
+		if(entityManagerFactory != null) return;
+			entityManagerFactory = Persistence
 				.createEntityManagerFactory("introducaojpa_pu");
-		
+
 	}
 	
 	public static void closeFactory(){
 		entityManagerFactory.close();
 	}
-
-	public static EntityManager getEntityManager(){
+	
+	public static EntityManager getEntityManager() {
 		return entityManagerFactory.createEntityManager();
-		
 	}
 }
